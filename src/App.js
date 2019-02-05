3f9750e3813117652e7a207386cddeaece79927a
Megan D <45672063+megan-d@users.noreply.github.com>
2019-02-04 17:32:20 -0700

    create todo item styling and check and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  //Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        //toggle true or false
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+}
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState( { todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
